9ed4352a5129a19faf4e4d547f07dfb1a9e9b4d7
DjNik <nikitos1235@gmail.com>
2021-06-24 12:37:58 +0300

    Added model to Solution

---
+++ ClassLibraryDB/Models/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDB
+{
+    public static class DB
+    {
+        public static CompFirmEntites CompFirm = new CompFirmEntites();
+    }
+}
