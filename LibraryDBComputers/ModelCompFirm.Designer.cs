355930bbd34f20a8d3fd9752e4d61edf0eddfcae
DjNik <nikitos1235@gmail.com>
2021-06-25 13:12:24 +0300

    Updated Connection String)

--- LibraryDBComputers/ModelCompFirm.Designer.cs
+++ LibraryDBComputers/ModelCompFirm.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\DjNik\Source\Repos\VorontsovNikita\ComputerFirm_Vorontsov_N.A_3802-2.0\LibraryDBComputers\ModelCompFirm.edmx" включено. 
+﻿// Создание кода T4 для модели "D:\RepoS\ComputerFirm_Vorontsov_N.A_3802-2.0\LibraryDBComputers\ModelCompFirm.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
